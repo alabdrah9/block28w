efd7ae5d3f5b6fa143dd466d7b54d376b10c9f83
alabdrah <ahmed9@Ahmeds-Air.lan>
2024-02-09 22:56:04 -0500

    trying to push the work

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -22,9 +22,9 @@ function App() {
       <div id="navbar"><link rel="stylesheet" href="./." /></div>
       <div id="main-section">
    <Routes>
-     <Route path="/blue" element={<h1>Blue</h1>} />
-     <Route path="/red" element={<h1>Red</h1>} />
-     <Route path="/home" element={<h1>Home</h1>} />
+     <Route path="/blue" element={<Blue>} />
+     <Route path="/red" element={<Red>} />
+     <Route path="/home" element={<Home>} />
    </Routes>
 </div>
     </div>
